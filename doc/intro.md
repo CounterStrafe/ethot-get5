d77d07fcfb7f6fc89a36010928ca4b8da4b0ffc8
Eric Oswald <eoswald39@gmail.com>
2020-07-13 18:01:31 -0400

    Start lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ethot-get5
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ethot_get5/core_test.clj
@@ -0,0 +1,7 @@
+(ns ethot-get5.core-test
+  (:require [clojure.test :refer :all]
+            [ethot-get5.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
